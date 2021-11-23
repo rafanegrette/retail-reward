861ca62ae1f5900e405b4d02e55690b33d8eb8f7
Rafael Negrette <rafanegrette@hotmail.com>
2021-11-23 13:13:29 -0500

    setup springboot project

---
+++ src/main/java/com/companyabc/retail/RetailRewardApplication.java
@@ -0,0 +1,13 @@
+package com.companyabc.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailRewardApplication.class, args);
+	}
+
+}
